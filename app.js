4d7bc8f8f671b18c507758a0f9e9a7043bb5c4aa
Ar273404 <arun273404@gmail.com>
2024-12-16 22:50:33 +0530

    p

--- app.js
+++ app.js
@@ -29,14 +29,29 @@ app.get('/', (req, res) => {
 //     credentials: true,
 //   })
 // );
-app.use(
-  cors({
-    origin: "*", // Allow all origins
-    methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"], // Allow specific headers
-    credentials: true, // Allow credentials
-  })
-);
+app.use((req, res, next) => {
+  const allowedOrigins = ["https://jobfrontend-qng1.onrender.com"];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, DELETE, PUT, OPTIONS"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
+  }
+
+  if (req.method === "OPTIONS") {
+    res.status(204).end(); // Handle preflight request
+    return;
+  }
+
+  next();
+});
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
